979c8c5ee65a29befb0db82fb4d94be644e8ecc6
Some Bad Scripts <64361203+BadMaliciousPyScripts@users.noreply.github.com>
2020-05-08 05:23:08 +0200

    Create copyit.py

---
+++ copyit.py
@@ -0,0 +1,3 @@
+## color array made by DevSudoku https://github.com/BadMaliciousPyScripts
+## c[0] = reset | c[1] = red | c[2] = green | c[3] = yellow | c[4] = blue | c[5] = magenta | c[6] = cyan
+c = ["\033[37m", "\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"]
